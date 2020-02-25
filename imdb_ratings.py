bc6f66e93a8cdf87a4781693ec3db1386483708f
J. Famous <joshua.famous@gmail.com>
2020-02-25 17:24:28 -0500

    Model evaluation and compilation

--- imdb_ratings.py
+++ imdb_ratings.py
@@ -15,12 +15,30 @@ print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU
 
 # Import data as 3 datasets, to train, validate, and test
 train_data, validation_data, test_data = tfds.load(
-    name="imdb_reviews", 
-    split=('train[:60%]', 'train[60%:]', 'test'),
-    as_supervised=True)
+	name="imdb_reviews", 
+	split=('train[:60%]', 'train[60%:]', 'test'),
+	as_supervised=True)
 
 # Use pre-trained text embedding from tensorflow hub
 embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-hub_layer = hub.KerasLayer(embedding, input_shape=[], 
-                           dtype=tf.string, trainable=True)
+hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
 hub_layer(train_examples_batch[:3])
+
+# Build network model
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+model.summary()
+
+# Loss function and compiler, using binary cross entropy to match sigmoid
+model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+
+history = model.fit(train_data.shuffle(10000).batch(512), epochs=20, validation_data=validation_data.batch(512), verbose=1)
+
+# Evaluation
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+	print("%s: %.3f" % (name, value))
