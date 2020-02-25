d3003ff3f461b4e19b519a33a93e651218451820
J. Famous <joshua.famous@gmail.com>
2020-02-25 17:19:17 -0500

    Add pre-trained model

--- imdb_ratings.py
+++ imdb_ratings.py
@@ -12,3 +12,15 @@ print("Version: ", tf.__version__)
 print("Eager mode: ", tf.executing_eagerly())
 print("Hub version: ", hub.__version__)
 print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+# Import data as 3 datasets, to train, validate, and test
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews", 
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+# Use pre-trained text embedding from tensorflow hub
+embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[], 
+                           dtype=tf.string, trainable=True)
+hub_layer(train_examples_batch[:3])
