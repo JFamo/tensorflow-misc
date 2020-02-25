3663f40bfdef75a9785feb5f982e170869eb71f1
J. Famous <joshua.famous@gmail.com>
2020-02-25 04:50:35 -0500

    Show prediction model

--- main.py
+++ main.py
@@ -8,16 +8,65 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__)
 
+# Import MNIST data
 fashion_mnist = keras.datasets.fashion_mnist
-
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# Define labels
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-               
+			   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 ##Show first training image
 ##plt.figure()
 ##plt.imshow(train_images[0])
 ##plt.colorbar()
 ##plt.grid(False)
 ##plt.show()
+
+# Scale to 0-1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+## Verify training data
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+	keras.layers.Flatten(input_shape=(28, 28)),
+	keras.layers.Dense(128, activation='relu'),
+	keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+			  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+			  metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+probability_model = tf.keras.Sequential([model,tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+# View prediction model
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+	plt.subplot(num_rows, 2*num_cols, 2*i+1)
+	plot_image(i, predictions[i], test_labels, test_images)
+	plt.subplot(num_rows, 2*num_cols, 2*i+2)
+	plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
