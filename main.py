d61c7f6f5ad26450f965ee327f7aa19c559b7770
J. Famous <joshua.famous@gmail.com>
2020-02-25 05:01:40 -0500

    Working clothing predictor

--- main.py
+++ main.py
@@ -54,6 +54,37 @@ probability_model = tf.keras.Sequential([model,tf.keras.layers.Softmax()])
 
 predictions = probability_model.predict(test_images)
 
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+# Prediction model functions
+def plot_image(i, predictions_array, true_label, img):
+	predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+	plt.grid(False)
+	plt.xticks([])
+	plt.yticks([])
+
+	plt.imshow(img, cmap=plt.cm.binary)
+
+	predicted_label = np.argmax(predictions_array)
+	if predicted_label == true_label:
+		color = 'blue'
+	else:
+		color = 'red'
+
+	plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],100*np.max(predictions_array),class_names[true_label]),color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+	predictions_array, true_label = predictions_array, true_label[i]
+	plt.grid(False)
+	plt.xticks(range(10))
+	plt.yticks([])
+	thisplot = plt.bar(range(10), predictions_array, color="#777777")
+	plt.ylim([0, 1])
+	predicted_label = np.argmax(predictions_array)
+
+	thisplot[predicted_label].set_color('red')
+	thisplot[true_label].set_color('blue')
+
 # View prediction model
 num_rows = 5
 num_cols = 3
@@ -67,6 +98,5 @@ for i in range(num_images):
 plt.tight_layout()
 plt.show()
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
+# Display final accuracy
 print('\nTest accuracy:', test_acc)
